16624c12f011cc5660204ab8ba31a241a5ca30c2
Md Ataullah <ataullahm100@gmail.com>
2024-05-01 17:21:26 +0600

    Navber design

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,18 +1,12 @@
-import { useContext, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import logo from '../../assets/logo.png';
-import { IoIosArrowForward, IoMdClose } from 'react-icons/io';
-import ProfileMenu from '../ProfileMenu/ProfileMenu';
-import { ContextAuth } from '../../provider/Provider';
-import userProfile from '../../assets/profile-blanck.png';
+import logo from '../../assets/logo.svg';
+import { IoIosArrowForward } from 'react-icons/io';
 import { IoClose } from 'react-icons/io5';
 import { MdMenu } from 'react-icons/md';
 import { Tooltip } from 'react-tooltip';
-import { FiUserPlus } from 'react-icons/fi';
+import { useState } from 'react';
 
 const Nav = () => {
-  const { userDta } = useContext(ContextAuth);
-  const [viewProfile, setViewProfile] = useState(false);
   const [view, setView] = useState(false);
 
   // console.log(hover);
@@ -20,113 +14,71 @@ const Nav = () => {
     <div className="">
       <Tooltip id="my-tooltip" />
       <div className="w-11/12 mx-auto max-w-[1700px]">
-        <div className=" navbar flex items-end flex-col sm:flex-row sm:justify-between">
-          <div className="navbar-start flex items-center flex-row-reverse justify-between w-full sm:w-auto sm:flex-row">
+        <div className=" navbar flex items-end flex-col md:flex-row md:justify-between">
+          <div className="navbar-start flex items-center flex-row-reverse justify-between w-full md:w-auto md:flex-row">
             <div className="flex gap-4 items-center">
-              {userDta ? (
-                <div className="navbar-end flex sm:hidden gap-4 w-auto">
-                  <img
-                    data-tooltip-id="my-tooltip"
-                    data-tooltip-content={userDta.displayName}
-                    data-tooltip-place="bottom-end"
-                    onClick={() => setViewProfile(!viewProfile)}
-                    src={userDta.photoURL}
-                    className="border-2 border-firstColor rounded-full h-12 w-12 cursor-pointer p-[2px]"
-                    alt=""
-                  />
-
-                  <div
-                    className={`absolute top-16 lg:top-20 right-8 duration-3000 transition-transform ${
-                      viewProfile
-                        ? 'translate-x-0 visible'
-                        : 'translate-x-96 invisible'
-                    }`}
-                  >
-                    <div className="relative">
-                      <div
-                        className="text-white absolute text-2xl top-3 left-3 border rounded cursor-pointer hover:border-firstColor duration-150"
-                        onClick={() => setViewProfile(!viewProfile)}
-                      >
-                        <IoMdClose />
-                      </div>
-                      <ProfileMenu />
+              <div className="relative dropdown">
+                <div
+                  onClick={() => setView(!view)}
+                  role="button"
+                  className="text-slate-900 md:hidden text-3xl"
+                >
+                  {view ? <IoClose /> : <MdMenu />}
+                </div>
+                {view && (
+                  <ul className="navManu absolute mt-1 md:mt-3 -ml-40 md:ml-4 z-10 p-2 shadow rounded-box w-52 border-2 border-solid border-firstColor bg-slate-800 text-white flex flex-col gap-2 md:hidden rounded-xl">
+                    <div
+                      onClick={() => setView(!view)}
+                      className="md:hidden cursor-pointer text-secondColor hover:text-primeryColor bg-slate-800 text-3xl -translate-x-10 translate-y-1/2 top-[calc(50%-20px)] absolute rounded-l-md border-l-2 border-y-2 border-firstColor"
+                    >
+                      <IoIosArrowForward />
                     </div>
-                  </div>
-                </div>
-              ) : (
-                <div className="relative dropdown">
-                  <div
-                    onClick={() => setView(!view)}
-                    role="button"
-                    className="text-white lg:hidden text-3xl"
-                  >
-                    {view ? <IoClose /> : <MdMenu />}
-                  </div>
-                  {view && (
-                    <ul className="navManu absolute mt-1 sm:mt-3 -ml-40 sm:ml-4 z-10 p-2 shadow rounded-box w-52 border-2 border-solid border-firstColor bg-slate-800 text-white flex flex-col gap-2 lg:hidden">
-                      <div
-                        onClick={() => setView(!view)}
-                        className="sm:hidden cursor-pointer text-secondColor hover:text-firstColor bg-slate-800 text-3xl -translate-x-10 translate-y-1/2 top-[calc(50%-20px)] absolute rounded-l-md border-l-2 border-y-2 border-firstColor"
-                      >
-                        <IoIosArrowForward />
-                      </div>
-                      <div
-                        style={{ borderRadius: '0 10px 0px 10px' }}
-                        onClick={() => setView(!view)}
-                        className="hidden sm:block cursor-pointer  text-firstColor text-3xl -top-4  right-0 translate-y-1/2 absolute bg-firstColor"
-                      >
-                        <span className="text-white">
-                          <IoMdClose />
-                        </span>
-                      </div>
-                      <NavLink
-                        to={'/'}
-                        className="hover:bg-slate-300 hover:text-firstColor py-1 px-4 rounded"
-                        onClick={() => setView(!view)}
-                      >
-                        Home
-                      </NavLink>
-                      <NavLink
-                        to={'/all-art-craft-items'}
-                        className="hover:bg-slate-300 hover:text-firstColor py-1 px-4 rounded"
-                        onClick={() => setView(!view)}
-                      >
-                        All Art & craft Items
-                      </NavLink>
-                      <NavLink
-                        to={'/add-craft-item'}
-                        className="hover:bg-slate-300 hover:text-firstColor py-1 px-4 rounded"
-                        onClick={() => setView(!view)}
-                      >
-                        Add Craft Item
-                      </NavLink>
-                      <NavLink
-                        to={'/my-art-craft-list'}
-                        className="hover:bg-slate-300 hover:text-firstColor py-1 px-4 rounded"
-                        onClick={() => setView(!view)}
-                      >
-                        My Art&Craft List
-                      </NavLink>
-                      <div className="flex sm:hidden flex-col gap-2 mt-2">
-                        <Link
-                          to={'/login'}
-                          className="font-semibold hover:bg-firstColor hover:text-white px-5 sm:px-8 py-2 sm:py-3 rounded-md border-2 border-white text-white active:scale-90 duration-150"
-                          onClick={() => setView(!view)}
-                        >
-                          Login
-                        </Link>
-                        <Link
-                          to={'/register'}
-                          className="font-semibold bg-firstColor hover:bg-white hover:text-firstColor text-white px-5 sm:px-8 py-2 sm:py-3 rounded-md active:scale-90 duration-150"
-                          onClick={() => setView(!view)}
-                        >
-                          Register
-                        </Link>
-                      </div>
-                    </ul>
-                  )}
-                </div>
-              )}
+                    <NavLink
+                      to={'/'}
+                      className="hover:bg-slate-300 hover:text-primeryColor py-1 px-4 rounded"
+                      onClick={() => setView(!view)}
+                    >
+                      Home
+                    </NavLink>
+                    <NavLink
+                      to={'/about'}
+                      className="hover:bg-slate-300 hover:text-primeryColor py-1 px-4 rounded"
+                      onClick={() => setView(!view)}
+                    >
+                      About
+                    </NavLink>
+                    <NavLink
+                      to={'/services'}
+                      className="hover:bg-slate-300 hover:text-primeryColor py-1 px-4 rounded"
+                      onClick={() => setView(!view)}
+                    >
+                      Services
+                    </NavLink>
+                    <NavLink
+                      to={'/blog'}
+                      className="hover:bg-slate-300 hover:text-primeryColor py-1 px-4 rounded"
+                      onClick={() => setView(!view)}
+                    >
+                      Blog
+                    </NavLink>
+                    <NavLink
+                      to={'/contact'}
+                      className="hover:bg-slate-300 hover:text-primeryColor py-1 px-4 rounded"
+                      onClick={() => setView(!view)}
+                    >
+                      Contact
+                    </NavLink>
+
+                    <Link
+                      to={'/register'}
+                      className="font-semibold bg-slate-300 hover:bg-slate-100 text-primeryColor px-5 md:px-8 py-2 md:py-3 rounded-md active:scale-90 duration-150"
+                      onClick={() => setView(!view)}
+                    >
+                      Appointment
+                    </Link>
+                  </ul>
+                )}
+              </div>
             </div>
             {/* Logo */}
             <Link
@@ -137,82 +89,48 @@ const Nav = () => {
             </Link>
           </div>
           {/* Learge screen Menu  */}
-          <div className="navbar-center hidden lg:flex">
-            <ul className="navManu menu menu-horizontal px-1 flex gap-2 xl:gap-6 text-white">
+          <div className="navbar-center hidden md:flex">
+            <ul className="navManu menu menu-horizontal px-1 flex gap-2 xl:gap-6 text-slate-800 font-semibold">
               <NavLink
                 to={'/'}
-                className="-skew-x-[15deg] w-28 py-2 border-2 px-3 text-center font-normal text-white rounded-md hover:border-firstColor"
+                className="border-b border-transparent hover:border-primeryColor hover:text-primeryColor px-2"
               >
                 Home
               </NavLink>
+
               <NavLink
-                to={'/all-art-craft-items'}
-                className="-skew-x-[15deg] py-2 border-2 px-3 text-center font-normal text-white rounded-md hover:border-firstColor"
+                to={'/about'}
+                className="border-b border-transparent hover:border-primeryColor hover:text-primeryColor px-2"
               >
-                All Art & craft Items
+                About
               </NavLink>
+
               <NavLink
-                to={'/add-craft-item'}
-                className="-skew-x-[15deg] py-2 border-2 px-3 text-center font-normal text-white rounded-md hover:border-firstColor"
+                to={'/services'}
+                className="border-b border-transparent hover:border-primeryColor hover:text-primeryColor px-2"
               >
-                Add Craft Item
+                Services
               </NavLink>
+
               <NavLink
-                to={'/my-art-craft-list'}
-                className="-skew-x-[15deg] py-2 border-2 px-3 text-center font-normal text-white rounded-md hover:border-firstColor"
+                to={'/blog'}
+                className="border-b border-transparent hover:border-primeryColor hover:text-primeryColor px-2"
               >
-                My Art&Craft List
+                Blog
+              </NavLink>
+
+              <NavLink
+                to={'/contact'}
+                className="border-b border-transparent hover:border-primeryColor hover:text-primeryColor px-2"
+              >
+                Contact
               </NavLink>
             </ul>
           </div>
           <div className="navbar-end flex items-center gap-2 w-40">
-            {userDta ? (
-              <div className="  hidden sm:flex gap-4">
-                <img
-                  onClick={() => setViewProfile(!viewProfile)}
-                  src={userDta.photoURL ? userDta.photoURL : userProfile}
-                  className="border-2 border-firstColor rounded-full h-12 w-12 cursor-pointer p-[2px]"
-                  alt=""
-                  data-tooltip-id="my-tooltip"
-                  data-tooltip-content={userDta.displayName}
-                  data-tooltip-place="bottom"
-                />
-
-                <div
-                  className={`absolute top-16 lg:top-20 right-16 duration-3000 transition-transform ${
-                    viewProfile
-                      ? 'translate-x-0 visible'
-                      : 'translate-x-96 invisible'
-                  }`}
-                >
-                  <div className="relative">
-                    <div
-                      className="text-white absolute text-2xl top-3 left-3 border rounded cursor-pointer hover:border-firstColor duration-150"
-                      onClick={() => setViewProfile(!viewProfile)}
-                    >
-                      <IoMdClose />
-                    </div>
-                    <ProfileMenu
-                      viewProfile={viewProfile}
-                      setViewProfile={setViewProfile}
-                    />
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <div className="hidden sm:block">
-                <button
-                  onClick={() =>
-                    document.getElementById('my_modal_1').showModal()
-                  }
-                  className="font-semibold bg-firstColor px-5 xl:px-8 rounded-md active:scale-90 duration-100 text-white button2"
-                >
-                  <span className="flex items-center gap-2">
-                    <FiUserPlus /> Account
-                  </span>
-                </button>
-              </div>
-            )}
+            <button className="hidden md:block font-semibold bg-transparent hover:bg-primeryColor px-3 xl:px-4 rounded-md active:scale-90 duration-150 text-primeryColor border border-primeryColor hover:text-white py-2">
+              Appointment
+            </button>
           </div>
         </div>
       </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-9xl">This is home page </h1>
+      <h1 className="text-9xl pt-96">This is home page </h1>
     </div>
   );
 };
