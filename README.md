c365ea69c76c93d2d9eb6b2b8a7c284c63187185
Md Ataullah <ataullahm100@gmail.com>
2024-05-03 18:13:33 +0600

    Data Update

--- README.md
+++
@@ -1,3 +0,0 @@
-# Snowy CareCare
-
-## Live Link:
--- src/Pages/About/About.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <div>
-      <h1 className="text-9xl pt-44">This is about page </h1>
-    </div>
-  );
-};
-
-export default About;
